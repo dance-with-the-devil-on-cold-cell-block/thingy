a7e24c7cff871dc4701e85dcedb36134edf86716
Friðrik Valur <val99f@gmail.com>
2023-11-06 11:09:13 +0000

    smá init á nextjs dóti

---
+++ shlist/src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Login | Shlist',
+  description: '',
+}
+
+
+export default function Home() {
+  return (<>
+    <h1>Log In Now Maybe?</h1>
+    </>
+  )
+}
---
+++ shlist/src/app/loginPrompt.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+
+export default function LoginPrompt {
+    return (<>
+        <Link href="">Sign in</Link>
+        <Link href="">Register</Link>
+        </>
+    )
+}
