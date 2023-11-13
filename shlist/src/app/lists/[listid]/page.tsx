cc767ebaeaa2360b3d01e5058f46a6a779778183
Friðrik Valur <val99f@gmail.com>
2023-11-13 12:34:29 +0000

    betrumbætti lista smá

--- shlist/src/app/lists/[listid]/page.tsx
+++ shlist/src/app/lists/[listid]/page.tsx
@@ -1,8 +1,12 @@
 import { notFound } from "next/navigation"
+import dummydata from "../../../../public/dummydata.json"
+import Listi from "./Listi"
+import { Metadata } from "next"
 
 export default function Page({ params }: { params: { listid: number } }) {
-    if(params.listid == 2) {
-    return params.listid}
+    const listanumer = (Number(params.listid)-1);
+    if(dummydata.dumdata.lists[listanumer].listId == listanumer+1) {
+    return (<Listi id={dummydata.dumdata.lists[listanumer].listId} name={dummydata.dumdata.lists[listanumer].nameL}/>)}
     else{
         notFound()
     }
