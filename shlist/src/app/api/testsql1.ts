32bcf3e74da222c8ef6c467f9346bc419dac2588
Myuoh <einarthor2901@gmail.com>
2023-11-16 08:41:11 +0000

    byrjun á setuppi fyrir crud föll
    á ennþá eftir að gerja allt rétt

--- shlist/src/app/api/testsql1.ts
+++ shlist/src/app/api/testsql1.ts
@@ -6,12 +6,35 @@ let cn = mysql2.createConnection({
     port:"3306",
     user: 'root',
     password: '12345',
-    database: 'pogger',
-    rowsAsArray: true,
+    database: 'pogger'
 });
-cn.query('select * from users;', function (_err:any, rows:any) {
+// C R U D
+// Create = Set
+// Read = Get
+// Update = upd
+// delete = Del
+
+/*
+    firstnam VARCHAR(15),
+    lastname varchar(15),
+    email VARCHAR(100),
+    passwd VARCHAR(255),
+*/
+function setUser(fn:string, ln:string, em:string, pw:string) {
+    cn.query('insert into users(firstnam,lastname,email,passwd) values (?,?,?,?)',[fn,ln,em,pw])
+}
+
+function getUsers() {
+    cn.query('select * from users;', function (_err:any, rows:any) {
     console.log(rows);
-});
+});}
+
+
+
 cn.query('show tables from pogger;', function (_err:any, rows:any) {
     console.log(rows);
 });
+
+setUser('pogger','dogger','ploggsmoller@ittail.dog', 'ég232323kannekki');
+
+getUsers();
