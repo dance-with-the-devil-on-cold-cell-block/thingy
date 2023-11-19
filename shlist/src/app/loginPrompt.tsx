60a90cd8a844c0ee705ce96ecde11e94d652d88c
vValdimar <valdimarkr21@tskoli.is>
2023-11-19 18:54:42 +0000

    hey

--- shlist/src/app/loginPrompt.tsx
+++ shlist/src/app/loginPrompt.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function LoginPrompt() {
     return (<>
-    <div>
-        <h1>Shlist</h1>
-        <Link href="/login">Log in</Link>
+    <div className="bg-darkPrimary justify-center text-center">
+        <h1 className="text-4xl font-medium p-10">Shlist</h1>
+        <Link href="/login"><div className="justify-center px-10 py-2">Log in</div></Link>
         <Link href="/register">Register</Link>
     </div>
         </>
