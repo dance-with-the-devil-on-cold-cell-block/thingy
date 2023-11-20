fc865a06dee3ccc2e81d065384f80e7d38d8e2e7
vValdimar <valdimarkr21@tskoli.is>
2023-11-20 10:11:11 +0000

    ok

--- shlist/src/app/loginPrompt.tsx
+++ shlist/src/app/loginPrompt.tsx
@@ -4,7 +4,9 @@ export default function LoginPrompt() {
     return (<>
     <div className="mx-auto px-60 lg:grid grid-cols-2 bg-darkPrimary justify-center text-center">
         <h1 className="col-span-2 text-4xl font-medium p-10">Shlist</h1>
-        <Link href="/login">Log in</Link>
+        <div className="flex justify-center">
+            <Link href="/login"><div className="w-40 rounded-full text-center p-3 text-xl text-black font-medium bg-white hover:bg-gray-200 shadow">Log in</div></Link>
+        </div>
         <Link href="/register">Register</Link>
     </div>
         </>
