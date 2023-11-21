ae7da91f4b2af64c67092e2a0b2a0319b4ded905
vValdimar <valdimarkr21@tskoli.is>
2023-11-21 13:27:55 +0000

    breytti bakgrunnalit

--- shlist/src/app/loginPrompt.tsx
+++ shlist/src/app/loginPrompt.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function LoginPrompt() {
     return (<>
-    <div className="h-screen mx-auto px-60 mw-60 lg:grid grid-cols-2 bg-darkPrimary justify-center text-center">
+    <div className="mx-auto px-60 mw-60 lg:grid grid-cols-2 justify-center text-center">
         <h1 className="col-span-2 text-4xl font-medium p-10">Shlist</h1>
         <div className="flex justify-center m-5">
             <Link href="/login"><div className="w-40 rounded-full text-center p-3 text-xl text-black font-medium bg-white hover:bg-gray-200 shadow">Log in</div></Link>
