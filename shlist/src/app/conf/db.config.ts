c0674f8f160928223786da750c88ebb61d3a0d22
Myuoh <einarthor2901@gmail.com>
2023-11-21 14:21:04 +0000

    updataedad

--- shlist/src/app/conf/db.config.ts
+++ shlist/src/app/conf/db.config.ts
@@ -1,7 +1,7 @@
 import mysql from 'mysql2/promise';
 
 const connection = mysql.createPool({
-    host: 'mail.23spisemaendeneogemil.is',
+    host: 'spisql.23spisemaendeneogemil.is',
     port: 3306,
     user: 'root',
     password: '5WagShit!', //virkar bara á tskola netinu LOLOLOLOLOLOLOLOLOLOL
