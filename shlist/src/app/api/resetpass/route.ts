bac3262028804969e9035604c30edddc002bce99
Myuoh <einarthor2901@gmail.com>
2023-12-05 14:05:54 +0000

    my b original gangster frilli

---
+++ shlist/src/app/api/resetpass/route.ts
@@ -0,0 +1,21 @@
+import connection from "@/app/conf/db.config";
+import { error } from "console";
+
+export async function POST(req: Request){
+    try {
+        const data = await req.json();
+        const [rows] = await connection.query(`call resetpass('${data.email}','${data.passwd}')`);
+        return new Response(JSON.stringify({ data: rows }), {
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          status: 200
+        });
+      }
+    catch{error}{
+        console.error('Database query failed:', error);
+        return new Response(JSON.stringify({ error: 'Internal Server Error' }), {
+          status: 500
+        });
+    }
+}
