4a84446a34b31b388a0a4da26cd1bee5c3d4ce86
Friðrik Valur <val99f@gmail.com>
2023-12-11 09:19:36 +0000

    gerði stöff

--- shlist/src/app/[lang]/lists/[listid]/Listi.tsx
+++ shlist/src/app/[lang]/lists/[listid]/Listi.tsx
@@ -7,6 +7,7 @@ export default function Listi({id}: {id:number}) {
     const [isLoading, setLoading] = useState(true)
    
     useEffect(() => {
+        function getListshit(){
         fetch("http://localhost:3000/api/listdeetz", {
             method:"POST",
             headers: {'Content-Type': 'application/json'},
@@ -16,7 +17,10 @@ export default function Listi({id}: {id:number}) {
         .then((data) => {
           setData(data)
           setLoading(false)
-        })
+          setTimeout(getListshit, 30000)
+        })}
+        getListshit()
+
     }, [])
     if (isLoading) return <p>Loading...</p>
     if (!data) return <p>No profile data</p>
@@ -26,7 +30,7 @@ export default function Listi({id}: {id:number}) {
     <div className="mx-auto w-[75%] xl:w-[60em] bg-primary font-medium border-2 border-black/20 rounded-md shadow-md">
     <h2 className="border-b-4 border-darkPrimary bg-primary rounded-t text-white p-2">Shopping List Items:</h2>
     <ul>
-        { Boolean(data.data[0][0].itemsDetails) ? (JSON.parse(data.data[0][0].itemsDetails).map((hlutur:any) => {console.log(hlutur);return(<li key={hlutur.id}><div className="bg-white hover:bg-white/90 text-black/90 border-b-2 border-black/20 p-2">{hlutur.name}: {hlutur.count}</div></li>)})) : <p>yo</p>}
+        { Boolean(data.data[0][0].itemsDetails) ? (JSON.parse(data.data[0][0].itemsDetails).map((hlutur:any) => {console.log(hlutur);return(<li key={hlutur.id}><div className="bg-white hover:bg-white/90 text-black/90 border-b-2 border-black/20 p-2">{hlutur.name}: {hlutur.count}</div></li>)})) : <li><div className="bg-white hover:bg-white/90 text-black/90 border-b-2 border-black/20 p-2">Ekkert í lista!</div></li>}
       <li><div className="rounded-b bg-white/90 hover:bg-white/80 text-black/80 p-2">+ Add item</div></li>
     </ul>
         </div>
--- shlist/src/app/[lang]/lists/[listid]/page.tsx
+++ shlist/src/app/[lang]/lists/[listid]/page.tsx
@@ -1,15 +1,42 @@
 import { notFound } from "next/navigation"
 import Listi from "./Listi"
-import { Metadata } from "next"
-
-export const metadata: Metadata = {
-    title:"listi!",
-    description:"",
+import type { Metadata, ResolvingMetadata } from 'next'
+ 
+type Props = {
+  params: { listid: number }
+}
+ 
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  // read route params
+  const id = params.listid
+ 
+  // fetch data
+  const product = await fetch("http://localhost:3000/api/listdeetz", {
+    method:"POST",
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({id: id})})
+    .then((res) => res.json());
+    if(product.data[0].length === 0) {
+      return {
+        title: "Not Found | Shlist",
+        openGraph: {
+          title:"Listamynd",
+          description:"yo"
+        },
+      }
+    }
+  // optionally access and extend (rather than replace) parent metadata
+  return {
+    title: product.data[0][0].nameL + " | Shlist",
     openGraph: {
       title:"Listamynd",
       description:"yo"
-    }
+    },
   }
+}
 
 export default function Page({ params }: { params: { listid: number } }) {
     return (<>
