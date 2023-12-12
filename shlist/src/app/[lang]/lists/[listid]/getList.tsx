3cc19f1646aacf0b77f7927d86767ebbdf3b8827
Friðrik Valur <val99f@gmail.com>
2023-12-12 13:01:06 +0000

    Update getList.tsx

--- shlist/src/app/[lang]/lists/[listid]/getList.tsx
+++ shlist/src/app/[lang]/lists/[listid]/getList.tsx
@@ -1,5 +1,6 @@
 export default async function getList(id:number) {
     const deets = await fetch("http://localhost:3000/api/listdeetz", {
+        mode:'no-cors',
             method:"POST",
             headers: {'Content-Type': 'application/json'},
             next: { revalidate:60 },
