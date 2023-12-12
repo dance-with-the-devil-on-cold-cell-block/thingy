d422c5bbfcc083cf25fb114ce25990fbd65011b4
Myuoh <einarthor2901@gmail.com>
2023-12-12 02:10:32 +0000

    Update route.ts

--- shlist/src/app/api/addlist/route.ts
+++ shlist/src/app/api/addlist/route.ts
@@ -2,7 +2,7 @@ import connection from "@/app/conf/db.config";
 
 export async function POST(req: Request){
         const data = await req.json();
-        const [rows] = await connection.query(`call addlist('${data.laname}','${data.uid}')`);
+        const [rows] = await connection.query(`call addlist('${data.laname}',${data.uid})`);
         return new Response(JSON.stringify({ data: rows }), {
           headers: {
             'Content-Type': 'application/json'
