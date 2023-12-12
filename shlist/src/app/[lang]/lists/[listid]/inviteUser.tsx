a7028a4fc5e0a0de9ef5580aea2aba16cac5ae64
Friðrik Valur <val99f@gmail.com>
2023-12-12 10:02:59 +0000

    ég hata linting and checking validity of types

--- shlist/src/app/[lang]/lists/[listid]/inviteUser.tsx
+++ shlist/src/app/[lang]/lists/[listid]/inviteUser.tsx
@@ -1,7 +1,7 @@
 "use server"
 import { cookies } from "next/headers";
 
-export default async function addAnItem(invemail:string, listid:string) {
+export default async function addAnItem(invemail:string, listid:number) {
     const swag = await fetch("http://localhost:3000/api/adduseer", {
         method:"POST",
         body: JSON.stringify({
