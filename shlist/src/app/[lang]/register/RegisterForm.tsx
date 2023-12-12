6906a2fb6919906f2815141683d9ecd72dd0d574
Friðrik Valur <val99f@gmail.com>
2023-12-12 13:20:12 +0000

    autocomplete

--- shlist/src/app/[lang]/register/RegisterForm.tsx
+++ shlist/src/app/[lang]/register/RegisterForm.tsx
@@ -36,9 +36,9 @@ export default function RegisterForm(){
         <label className="col-span-2 md:col-span-1">Eftirnafn</label>
         <input className="col-span-2 md:col-span-1" type="text" name="nafn" value={lastnafnValue} onChange={(e) => setLastnafn(e.target.value)} required/>
         <label className="col-span-2 md:col-span-1">Email</label>
-        <input className="col-span-2 md:col-span-1" type="email" name="email" value={emailValue} onChange={(e) => setEmail(e.target.value)} required/>
+        <input className="col-span-2 md:col-span-1" type="email" name="email" autoComplete="email" value={emailValue} onChange={(e) => setEmail(e.target.value)} required/>
         <label className="col-span-2 md:col-span-1">Password</label>
-        <input className="col-span-2 md:col-span-1" type={showPassword ? "text" : "password"} name="pw" value={pwValue} onChange={(e) => setPw(e.target.value)} required/>
+        <input className="col-span-2 md:col-span-1" type={showPassword ? "text" : "password"} autoComplete="password" name="pw" value={pwValue} onChange={(e) => setPw(e.target.value)} required/>
         <div className="cursor-pointer select-none" onClick={(e) => {setPasswordVisibility(!showPassword)}}>Show Password!!</div>
         <label className="col-span-2 md:col-span-1">Repeat Password</label>
         <input className="col-span-2 md:col-span-1" type={showPassword2 ? "text" : "password"} name="pw2"  value={pw2Value} onChange={(e) => setPw2(e.target.value)} required/>
